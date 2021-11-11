0374036d66f9dcbe2705816c134e3692af9408ea
JanezSajovic <90645762+JanezSajovic@users.noreply.github.com>
2021-11-11 18:00:34 +0100

    Unix Test projekt | Test Get in Gets | ŠE ne deluje pravilno | Readme

---
+++ MicrocopNalogaV2/MicrocopNaloga.Tests/UsersControlerTest.cs
@@ -0,0 +1,109 @@
+﻿using FakeItEasy;
+using MicrocopNalogaV2;
+using MicrocopNalogaV2.Controllers;
+using System;
+using Xunit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using MicrocopNalogaV2.Models.Models;
+
+namespace MicrocopNaloga.Tests
+{
+    public class UsersControlerTest
+    {
+        public UserController _controller;
+        public IUserRepository _context;
+        public ILogger<UserController> _logger;
+        public IConfiguration _config;
+
+        public UsersControlerTest(IUserRepository userRepository, IConfiguration config, ILogger<UserController> logger)
+        {
+            _logger = logger;
+            _config = config;
+            _context = userRepository;
+            _controller = new UserController(_context, _config, _logger);
+
+        }
+
+        [Fact]
+        public void GetAllUsersTest()
+        {
+            // Arange
+            // Act
+            var result = _controller.Gets();
+            Assert.IsType<OkObjectResult>(result.Result);
+            var list = result.Result as OkObjectResult;
+            Assert.IsType<List<UserModel>>(list.Value);
+            var listBooks = list.Value as List<UserModel>;
+
+            // Pričakujemo da je v bazi 5 uporabnikov
+            Assert.Equal(5, listBooks.Count);
+        }
+
+        // Test s katerim pričakujemo da se pod ID=1 nahajajo podatki o Cirilu Kosmaču
+        // ID = 99 pa uporabnik ne obstaja 
+        [Theory]
+        [InlineData(1, 99)]
+        public void GetFirstUserById(int trueId, int fakeId)
+        {
+            // Arange
+            var validId = trueId;
+            var invalidId = fakeId;
+
+            // Act
+            var notFoundResult = _controller.Get(validId);
+            var okResult = _controller.Get(fakeId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(notFoundResult.Result);
+            Assert.IsType<OkObjectResult>(okResult.Result);
+
+            var item = okResult.Result as OkObjectResult;
+
+            Assert.IsType<UserModel>(item.Value);
+
+            var user = item.Value as UserModel;
+            Assert.Equal(trueId, user.Id);
+            Assert.Equal("CirilK", user.UserName);
+            Assert.Equal("ciril.k@gmail.com", user.Email);
+            Assert.Equal("Ciril Kosmač", user.FullName);
+            Assert.Equal("017541474", user.PhoneNumber);
+            Assert.Equal("slovenščina", user.Language);
+            Assert.Equal("si", user.Culture);
+            Assert.True(user.IsValidated);
+
+        }
+
+        [Fact]
+        public void AddUserTest() {
+            //OK RESULT TEST START
+
+            //Arrange
+            var user = new UserMiniModel()
+            {
+                UserName = "KLovro",
+                Email = "lovro.kuhar@gmail.com",
+                Language = "slovenščina",
+                Culture = "si",
+                Password = "Kurah123!",
+                PhoneNumber = "030888555"                           
+            };
+
+            //Act
+            var createdResponse = _controller.Create(user);
+
+            //Assert
+            Assert.IsType<UserModel>(createdResponse);
+
+            //value of the result
+            //var item = createdResponse as UserModel;
+            //Assert.IsType<UserModel>(item.Value);
+
+            //check value of this book
+            //var tempUser = item.Value as UserModel;
+            //Assert.Equal(user.Author, bookItem.Author);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -43,7 +43,7 @@ Primer izpisa v .log datoteki
 
 | Uporabniško ime      | Geslo |
 | -------------------- | ----------- |
-| JureAdmin      | admin123       |
+| JureAdmin      | Admin123!       |
 | DomenAdmin   | Nepovemtigesla123!        |
 | ToneAdmin   | SkrivnoGeslo!321!        |
 
@@ -52,10 +52,12 @@ Primer izpisa v .log datoteki
 
 | ID | Uporabniško ime | Geslo | Celo ime | Email | Telefon | Jezik | Kultura | Validiran |
 | - | - | - | - | - | - | - | - | - |
-| 1 | CirilK | cirilko123 | Ciril Kosmač | ciril.k@gmail.com | 017541474 | slovenščina | si | true |
-| 2 | ABoris | borisanovak! | Boris A. Novak | novakov.boris@gmail.com | 030698747 | slovenščina | si | false |
-| 4 | JKRowling | parryhotter12 | Joanne Rowling | Joanne.Rowling@gmail.com | 0802288 | angleščina | en | true |
-| 5 | Prešeren | kam123!! | France Prešeren | povodni.moz@gmail.com | 040187968 | slovenščina | si | false|
+| 1 | CirilK | Cirilko123! | Ciril Kosmač | ciril.k@gmail.com | 017541474 | slovenščina | si | true |
+| 2 | ABoris | Borisanovak4! | Boris A. Novak | novakov.boris@gmail.com | 030698747 | slovenščina | si | false |
+| 4 | JKRowling | parryHotter12! | Joanne Rowling | Joanne.Rowling@gmail.com | 0802288 | angleščina | en | true |
+| 5 | Prešeren | Kam123!! | France Prešeren | povodni.moz@gmail.com | 040187968 | slovenščina | si | false|
 
 ####
+
+Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzY2NDMyNDksImlzcyI6IlRlc3QuY29tIiwiYXVkIjoiVGVzdC5jb20ifQ.ptjCSyFxuIJ1vkHUtV7VBKpxnLSDquQbZ1C97fiItss
 
